a0146fa5fc1541f8246842292114936a3ef63fc8
EC2 Default User <ec2-user@ip-172-31-35-153.ap-northeast-1.compute.internal>
2019-11-24 08:56:18 +0000

    add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_051135) do
+ActiveRecord::Schema.define(version: 2019_11_24_072726) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_11_20_051135) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", default: "", null: false
+    t.string "profile_photo", default: "default_profile_img.png"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
